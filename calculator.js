f24cfa17414bafe10d30490dbb8835deca91210c
Nicholas Crane <nickcrusher30@gmail.com>
2022-10-02 22:14:31 -0400

    Create display and number buttons

---
+++ calculator.js
@@ -0,0 +1,4 @@
+const calculator = document.querySelector('.calculator');
+
+const buttons = document.querySelector('.buttons');
+buttons.style.gridTemplateColumns = `repeat(${5}, auto)`;
---
+++ calculatorFunctions.js
@@ -0,0 +1,28 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    if (b === 0) {
+        console.log("RHEE DON'T BREAK MATH");
+        return null;
+    }
+
+    return a / b;
+}
+
+function operate(operator, a, b) {
+    if (operator === "+") return add(a, b);
+    if (operator === "-") return subtract(a, b);
+    if (operator === "*") return multiply(a, b);
+    if (operator === "/") return divide(a, b);
+    return null;
+}
