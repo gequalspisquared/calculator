b19f6897c01ecb3b3b522668975f844531a72ff7
Nicholas Crane <nickcrusher30@gmail.com>
2022-10-04 18:30:55 -0400

    Implement more logic, some bugs to fix

--- calculator.js
+++ calculator.js
@@ -1,4 +1,23 @@
+// global variables
+let eq = ''; // equation
+let ans = ''; // answer
+let num1 = '0';
+let num2 = '';
+let op = '';
+
+const ops = ['/', '*', '-', '+'];
+
+// grab important pieces from document
 const calculator = document.querySelector('.calculator');
 
-const buttons = document.querySelector('.buttons');
-buttons.style.gridTemplateColumns = `repeat(${5}, auto)`;
+const buttons = calculator.querySelector('.buttons');
+buttons.style.gridTemplateColumns = `repeat(${4}, auto)`;
+
+const display = document.querySelector('.display');
+const equation = display.querySelector('.equation');
+const btns = buttons.querySelectorAll('.btn');
+const answer = display.querySelector('.answer');
+answer.textContent = '0';
+
+// add event listeners
+addListeners();
--- calculatorFunctions.js
+++ calculatorFunctions.js
@@ -13,7 +13,7 @@ function multiply(a, b) {
 function divide(a, b) {
     if (b === 0) {
         console.log("RHEE DON'T BREAK MATH");
-        return null;
+        return 'RHEEE';
     }
 
     return a / b;
@@ -25,4 +25,53 @@ function operate(operator, a, b) {
     if (operator === "*") return multiply(a, b);
     if (operator === "/") return divide(a, b);
     return null;
+}
+
+function clearCalculator() {
+    num1 = NaN;
+    num2 = NaN;
+    op = null;
+    eq = '';
+    ans = '';
+}
+
+function processButton(e) {
+    const btn = e.target;
+    const pressed = btn.classList[1];
+
+    if (num2 !== '' && ops.includes(pressed)) {
+        ans = operate(op, num1, Number(num2));
+        num1 = ans;
+        num2 = '';
+        op = pressed;
+    }
+
+    if (pressed === '=') {
+        if (!num2) num2 = 0;
+        if (!op) op = '+';
+        num1 = Number(num1);
+        num2 = Number(num2);
+        ans = operate(op, num1, num2);
+    }
+
+    if (ops.includes(pressed)) {
+        num1 = Number(num1);
+        op = pressed;
+    }
+
+    if (op === '') {
+        if (num1 === '0') num1 = '';
+        num1 += pressed;
+    } 
+
+    if (typeof num1 === 'number' && !ops.includes(pressed) && pressed !== '=') {
+        num2 += pressed;
+    }
+
+    equation.textContent = num1 + op + num2;
+    answer.textContent = ans;
+}
+
+function addListeners() {
+    btns.forEach(btn => btn.addEventListener('click', processButton));
 }
